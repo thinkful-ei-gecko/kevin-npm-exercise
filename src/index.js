30cfe525a523ad6ce5d0763c5c78440a65384c9a
Kevin Wei <chenghaowei@gmail.com>
2019-08-28 13:01:40 -0400

    convert to modules, and build with webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-'use strict';
-/* global shoppingList, store, api */
+import './styles/index.css';
+import $ from 'jquery';
+import api from './scripts/api';
+import store from './scripts/store';
+import shoppingList from './scripts/shopping-list';
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
